eef81fd713dfb61a67e20d69ff5fca504ba58021
her <504105925@qq.com>
2020-05-20 14:26:59 +0800

    修复打包exe 运行不了的bug 在dist中添加 package.json 即可

--- vuepc-master/src/main.js
+++ vuepc-master/src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
@@ -10,6 +13,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
+  render: creator => creator(App)
 })
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
